b924156162bfe2706effefb394034523a43b8bd0
ASKadetov <kadetov93@gmail.com>
2026-05-24 13:15:55 +0700

    add script to init.sql

--- db/init.sql
+++ db/init.sql
@@ -35,3 +35,17 @@ ON option_snapshots(snapshot_ts);
 
 CREATE INDEX idx_instr_name
 ON option_snapshots (instrument_name);
+
+
+CREATE TABLE open_interest (
+    snapshot_ts        TIMESTAMP NOT NULL,
+    instrument_name    TEXT NOT NULL,
+    
+    oi                 DOUBLE PRECISION,
+    oiCcy              DOUBLE PRECISION,
+    oiUsd              DOUBLE PRECISION,
+    price              DOUBLE PRECISION,	
+
+    CONSTRAINT open_interest_pk 
+    PRIMARY KEY (snapshot_ts, instrument_name)
+);
